bab8ce708123edf609de16886f1448e4b996eaee
Konstantin <phpconf@gmail.com>
2014-08-06 15:33:10 +0400

    add setup py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scalafn',
+    version='0.0.1',
+    author='Konstantin Potapov',
+    author_email='phpconf@gmail.com',
+    packages=find_packages(),
+    license='BSD',
+    url='https://github.com/Frozen/scalafn',
+    description='Scala like list',
+    install_requires=[
+        "future"
+    ],
+    classifiers=[
+        'Development Status :: Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
